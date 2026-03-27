f173965c6b1e24ed5f12bfc667ec244d3123bd47
BachhavPritesh <pritesh.v.bachhav.cg@gmail.com>
2026-03-27 10:30:40 +0530

    Abstraction Defination and rules are implemented

---
+++ OOPS/Abstraction/abstraction.cpp
@@ -0,0 +1,28 @@
+// 🧠 Abstraction in C++
+
+// Abstraction means hiding internal implementation details and showing only the essential features of an object.
+
+// 👉 “User ko sirf what to do dikhana, how it works nahi.”
+
+// 🎯 Real-Life Example
+
+// Think of a car 🚗:
+
+// You use: steering, accelerator, brake
+// You don’t see: engine working, fuel injection, wiring
+
+// 👉 That’s abstraction.
+
+// 💻 Abstraction in C++
+
+// In C++, abstraction is achieved using:
+
+// Classes
+// Access specifiers (private, public)
+// Abstract classes (pure virtual functions)
+
+//Implement using "pure virtual function " then the class turn in abstraction class and we cannot create a oblect of
+//abstraction class
+
+//Abstraction class -> What to do? extended Children Class-> How to do ? also in children class we have to define Pure virtual function
+//In abstraction class -> Normal Datamembers + Function Members can be used
